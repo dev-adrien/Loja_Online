18ea80255e3f912e01fb82350f157028b6f7aa1a
Ikaro Rufino <ikarogust32@gmail.com>
2025-07-10 12:48:17 -0300

    Funções edit e delete movidas para a pagina principal
    Removi o componente Produtos.jsx da pagina principal e movi as funções edit e delete para o compoente Produto.jsx que está na Home.jsx,. Além disso coloquei um css no body pra não ter conflito com o modo escuro de alguns navegadores(como o meu)

--- src/PaginaInicial.jsx
+++ src/PaginaInicial.jsx
@@ -10,7 +10,6 @@ export default function PaginaInicial ()  {
     <div>
       <h1>Bem-vindo à nossa loja!</h1>
       <h2>Produtos disponíveis:</h2>
-      <Produtos />
       <Home />
       <button
         type="button"
--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Card, CardContent, CardMedia, Typography, Button, CardActionArea, CardActions } from '@mui/material';
 import ProdutoPopUp from './ProdutoPopUp';
+import { useNavigate } from 'react-router-dom';
 
 export default function Produto({ id, nome, preco, categoria, imagem, estoque, descricao }) {
   const [open, setOpen] = React.useState(false);
   const produto = { id, nome, preco, categoria, imagem, estoque, descricao };
+  const navigate = useNavigate();
 
   return (
     <>
@@ -19,8 +21,25 @@ export default function Produto({ id, nome, preco, categoria, imagem, estoque, d
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" sx={{ color: '#015FCA' }}>Editar</Button>
-          <Button size="small" color="secondary">DELETAR</Button>
+          <Button size="small" sx={{ color: '#015FCA' }} onClick={() => navigate(`/editar/${produto.id}`)}>Editar</Button>
+          <Button size="small" color="secondary" 
+            onClick={() => {
+              if (window.confirm("Tem certeza que deseja excluir este produto?")) {
+                fetch(`http://localhost:3000/produtos/${id}`, {
+                  method: "DELETE",
+                })
+                  .then((res) => {
+                    if (res.ok) {''
+                      alert("Produto excluído com sucesso!");
+                      navigate("/");
+                      if (onEditSuccess) onEditSuccess();
+                    } else {
+                      alert("Erro ao excluir o produto.");
+                    }
+                  });
+              }
+            }}
+          >DELETAR</Button>
         </CardActions>
       </Card>
       <ProdutoPopUp open={open} onClose={() => setOpen(false)} produto={produto} />
